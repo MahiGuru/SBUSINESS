092752fe15ca4250efb276c5128d95f8379619fc
Mahipal Gurjala <Mahipal.Gurjala@planonsoftware.com>
2019-04-12 15:32:22 +0530

    flex layout implementation done

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,13 +6,15 @@ import { LayoutComponent } from './layout.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MaterialModule } from '../ng-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [LayoutComponent, ToolbarComponent, FooterComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   exports: [LayoutComponent, ToolbarComponent, FooterComponent]
 })
