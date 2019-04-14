1504f1b9e8c5b9953b839335b2626b611f50c437
MahiGuru <mahi6535@gmail.com>
2019-04-15 02:09:02 +0530

    new row functionality implemnted

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -7,46 +7,54 @@ import { DatatableComponent } from '@swimlane/ngx-datatable';
   styleUrls: ['./inventory.component.scss']
 })
 export class InventoryComponent implements OnInit {
-  // rows = [
-  //   {
-  //     name: 'Austin', description: 'Male', itemType: 'Swimlane',
-  //     assemblyPartner: 'Male', received: 'Swimlane', committed: 'Male', adjustment: 'Swimlane'
-  //     , shipped: 'Swimlane'
-  //     , onhand: 'Swimlane'
-  //     , updated: 'Swimlane'
-  //   }
 
-  // ];
-  // columns = [
-  //   { prop: 'Item#', name: 'Item #' },
-  //   { name: 'Description' },
-  //   { name: 'Item Type' },
-  //   { name: 'Assembly Partner' },
-  //   { name: 'Received' },
-  //   { name: 'Committed' },
-  //   { name: 'Adjustment' },
-  //   { name: 'Shipped' },
-  //   { name: 'On-hand', prop: 'onhand' },
-  //   { name: 'Updated' }
-  // ];
+
+
   rows = [];
   filterVal: any;
   temp = [];
+  public newRowHeight: any = 100;
+  public newRow = {
+    id: '',
+    description: '',
+    itemType: '',
+    assemblyPartner: '',
+    received: '',
+    committed: '',
+    adjustment: '',
+    shipped: '',
+    onhand: '',
+    updated: ''
 
+  };
+  public addRows: any = {
+    id: '',
+    description: '',
+    itemType: '',
+    assemblyPartner: '',
+    received: '',
+    committed: '',
+    adjustment: '',
+    shipped: '',
+    onhand: '',
+    updated: ''
+  };
+
+  public addedRows = [];
   columns = [
-    { prop: 'id', name: 'Item #' },
-    { name: 'Description' },
-    { prop: 'itemType', name: 'Item Type' },
-    { prop: 'assemblyPartner', name: 'Assembly Partner' },
-    { name: 'Received' },
-    { name: 'Committed' },
-    { name: 'Adjustment' },
-    { name: 'Shipped' },
-    { name: 'On-hand', prop: 'onhand' },
-    { name: 'Updated' }
+    { prop: 'id', name: 'Item #', width: 100 },
+    { name: 'Description', width: 250 },
+    { prop: 'itemType', name: 'Item Type', width: 100 },
+    { prop: 'assemblyPartner', name: 'Assembly Partner', width: 100 },
+    { name: 'Received', width: 100 },
+    { name: 'Committed', width: 100 },
+    { name: 'Adjustment', width: 100 },
+    { name: 'Shipped', width: 100 },
+    { name: 'On-hand', prop: 'onhand', width: 100 },
+    { name: 'Updated', width: 100 }
   ];
   @ViewChild(DatatableComponent) table: DatatableComponent;
-
+  names: any;
   constructor() {
     this.fetch((data) => {
       // cache our list
@@ -56,12 +64,12 @@ export class InventoryComponent implements OnInit {
       console.log(data);
       this.rows = data;
     });
-  }
 
+  }
   fetch(cb) {
     const req = new XMLHttpRequest();
     req.open('GET', `assets/data/data.json`);
-
+    this.names = ['MAHIPAL', 'GURJALA'];
     req.onload = () => {
       cb(JSON.parse(req.response));
     };
@@ -80,6 +88,32 @@ export class InventoryComponent implements OnInit {
     this.rows = temp;
     // Whenever the filter changes, always go back to the first page
     this.table.offset = 0;
+  }
+  toggleExpandRow(row) {
+    this.rows.unshift(this.newRow);
+    this.rows = [...this.rows];
+
+    this.addedRows = [
+      ...this.addedRows, this.addRows
+    ];
+
+    setTimeout(() => {
+      this.table.rowDetail.toggleExpandRow(this.rows[0]);
+    }, 100);
+  }
+  addAnotherRow() {
+    console.log(this.addedRows);
+    this.addedRows = [
+      ...this.addedRows, this.addRows
+    ];
+    this.newRowHeight += 30;
+  }
+  removeCurrentRow(currentRow) {
+    console.log(this.addedRows.indexOf(currentRow));
+    this.addedRows.splice(this.addedRows.indexOf(currentRow), 1);
+  }
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
   }
   ngOnInit() {
 
