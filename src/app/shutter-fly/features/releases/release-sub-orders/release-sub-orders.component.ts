e4b9a14f63b7823f5e45ae548bfabe98187c81dc
Mahipal Gurjala <Mahipal.Gurjala@planonsoftware.com>
2019-05-29 15:48:32 +0530

    release page refactoring

--- src/app/shutter-fly/features/releases/release-sub-orders/release-sub-orders.component.ts
+++ src/app/shutter-fly/features/releases/release-sub-orders/release-sub-orders.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import * as _ from 'lodash';
 import {
   faWindowClose, faCheckSquare,
-  faPencilAlt, faTrashAlt
+  faPencilAlt, faTrashAlt, faShare, faBan, faPrint, faCheckCircle
 } from '@fortawesome/free-solid-svg-icons';
+import { PrintOrderService } from 'src/app/shutter-fly/shared/services/print-order.service';
+import { ReleasesService } from 'src/app/shutter-fly/shared/services/releases.service';
+import { ORDERS } from 'src/app/shutter-fly/core/models/order-state';
 
 @Component({
   selector: 'sb-release-sub-orders',
@@ -13,6 +16,7 @@ import {
 export class ReleaseSubOrdersComponent implements OnInit {
 
   @Input() row: any;
+  @Input() rows: any;
   @Input() table: any;
 
   @Output() rowsUpdate: EventEmitter<any> = new EventEmitter();
@@ -21,8 +25,13 @@ export class ReleaseSubOrdersComponent implements OnInit {
   faCheckSquare = faCheckSquare;
   faPencilAlt = faPencilAlt;
   faTrashAlt = faTrashAlt;
+  faShare = faShare;
+  faBan = faBan;
+  faPrint = faPrint;
+  faCheckCircle = faCheckCircle;
+  public OrdersState = ORDERS;
 
-  constructor() { }
+  constructor(public releaseService: ReleasesService) { }
 
   ngOnInit() {
   }
@@ -31,5 +40,14 @@ export class ReleaseSubOrdersComponent implements OnInit {
   }
   onSavePropertyVal(row) {
     row.editable = false;
+  }
+  updateOrderStatus(id, status) {
+    const orderRecord = [{
+      PrintOrderId: id,
+      Status: status
+    }];
+    this.releaseService.updateReleaseOrderStatus(orderRecord).subscribe(newRecords => {
+      console.log(newRecords);
+    });
   }
 }
